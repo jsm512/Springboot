3235e6a7e347b7a68c2e9407d31752821bcef2af
jsm512 <wlstmdals12@naver.com>
2024-07-23 14:56:51 +0900

    xml branch

--- jsondemo/src/main/java/com/example/jsondemo/JsondemoApplication.java
+++ jsondemo/src/main/java/com/example/jsondemo/JsondemoApplication.java
@@ -6,13 +6,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,49 +24,35 @@ public class JsondemoApplication {
 
 	@RestController
 	public static class ApiController {
-		private final String DATA_DIR = "data/input/users.json";
-		private final ObjectMapper objectMapper = new ObjectMapper();
+		private final String DATA_DIR = "data/xml/users.xml";
+		private final XmlMapper xmlMapper = new XmlMapper();
 
 		@GetMapping("/api/users")
 		public ResponseEntity<Object> getUsers(
-				@RequestParam(value = "page", defaultValue = "0") int page,
-				@RequestParam(value = "size", defaultValue = "10") int size,
-				@RequestParam(value = "sort", required = false) String sort) {
+				@RequestParam(value = "userName", required = false) String userName) {
+			if (userName == null) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(new ErrorDTO("Invalid data format"));
+			}
 			try {
 				ClassPathResource resource = new ClassPathResource(DATA_DIR);
-				List<User> users = objectMapper.readValue(
+				List<UserDTO> users = xmlMapper.readValue(
 						resource.getInputStream(),
-						new TypeReference<List<User>>() {
+						new TypeReference<List<UserDTO>>() {
 
 						});
-
-				List<User> sortedUsers = users.stream()
-						.skip(page * size)
-						.limit(size)
+				List<UserDTO> sortedUsers = users.stream()
+						.filter(user -> userName.equals(user.getUsername()))
 						.collect(Collectors.toList());
-				if (sort != null && !sort.trim().isEmpty()) {
-					sortedUsers = sortedUsers.stream()
-							.sorted(getComparator(sort))
-							.collect(Collectors.toList());
+				if (sortedUsers.isEmpty()) {
+					return ResponseEntity.status(HttpStatus.NOT_FOUND)
+							.body(new ErrorDTO("data not found"));
 				}
 				System.out.println(sortedUsers);
 				return ResponseEntity.ok(sortedUsers);
 			} catch (IOException e) {
 				e.printStackTrace();
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-			}
-		}
-
-		private Comparator<User> getComparator(String sort) {
-			switch (sort) {
-				case "username":
-					return Comparator.comparing(User::getUsername);
-				case "user_id":
-					return Comparator.comparingLong(User::getUser_id);
-				case "post_count":
-					return Comparator.comparingInt(User::getPost_count);
-				default:
-					return Comparator.comparing(User::getUsername);
 			}
 		}
 	}
---
+++ jsondemo/src/main/java/com/example/jsondemo/UserDTO.java
@@ -0,0 +1,58 @@
+package com.example.jsondemo;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(name = "users")
+public class UserDTO {
+
+    private int id;
+
+    private String username;
+
+    private String email;
+
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public UserDTO() {
+    }
+
+    public UserDTO(int id, String username, String email, String phone) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+}
